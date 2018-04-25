a5032cd01e706302dd2f03287d4bad51866da7d2
Jay R Bolton <jayrbolton@gmail.com>
2018-04-25 14:07:01 -0700

    Add styling and live dev server support

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["KBase SDK Docs"],titleterms:{app:[],builder:[],doc:0,document:[],indic:0,kbase:0,sdk:0,tabl:0}})
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["KBase SDK Docs"],titleterms:{doc:0,indic:0,kbase:0,sdk:0,tabl:0}})

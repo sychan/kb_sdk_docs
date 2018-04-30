074f260417ffd0ce21f138ade394fa777c117488
Jay R Bolton <jayrbolton@gmail.com>
2018-04-30 10:12:03 -0700

    Add support for markdown files

--- source/conf.py
+++ source/conf.py
@@ -54,7 +54,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -191,3 +191,8 @@ texinfo_documents = [
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+# Markdown support
+source_parsers = {
+   '.md': 'recommonmark.parser.CommonMarkParser',
+}
